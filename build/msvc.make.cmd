fe62427590c4eca75191f0fab73f2cfc43a8037f
Grigore Stefan <g_stefan@yahoo.com>
2021-03-15 12:37:28 +0200

    project-structure

--- build/msvc.make.cmd
+++ build/msvc.make.cmd
@@ -18,5 +18,5 @@ echo "Error: %ACTION%"
 exit 1
 :StepX
 
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-sha256.static.compile
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-sha256.dynamic.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/quantum-script-extension-sha256.static.compile
+call :cmdX xyo-cc --mode=%ACTION% @build/source/quantum-script-extension-sha256.dynamic.compile
--- build/msvc.sign.cmd
+++ build/msvc.sign.cmd
@@ -5,6 +5,6 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 echo -^> sign quantum-script-extension-sha256
 
-pushd bin
+pushd output
 for /r %%i in (*.dll) do call grigore-stefan.sign "Quantum Script Extension SHA256" "%%i"
 popd
